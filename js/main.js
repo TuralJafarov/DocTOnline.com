7006f4453c553649e20d2186719bf699929ce8ce
TuralJafarov <tural.cafarov@yahoo.com>
2023-10-16 22:48:14 +0400

    update

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $('.owl-list').owlCarousel({
                items:1
            },
            600:{
-               items:1
+               items:3
            },
            1000:{
                items:1
