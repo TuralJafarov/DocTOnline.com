f155c7fa474974583c33679c246e95597fa7fcff
TuralJafarov <tural.cafarov@yahoo.com>
2023-10-17 15:19:12 +0400

    update

--- js/main.js
+++ js/main.js
@@ -82,25 +82,3 @@ $('.owl-category').owlCarousel({
    });
 
 
-
-   $('.owl-list4').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-//     autoplay:true,
-//     autoplayTimeout:5000,
-//    autoplayHoverPause:true,
-   navText:["<img src='../image/next.png'>","<img src='../image/next.png'>"],
-       responsive:{
-           0:{
-               items:1
-           },
-           600:{
-               items:1
-           },
-           1000:{
-               items:1
-           }
-       }
-   });
-   
