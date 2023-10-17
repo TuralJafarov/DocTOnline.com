c4c776f420f0a5e024617004caee5d532cc03def
TuralJafarov <tural.cafarov@yahoo.com>
2023-10-17 11:17:54 +0400

    update

--- js/main.js
+++ js/main.js
@@ -19,26 +19,25 @@ $('.owl-first').owlCarousel({
 
 
 
+
 $('.owl-list').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
-//     autoplay:true,
-//     autoplayTimeout:5000,
-//    autoplayHoverPause:true,
-   navText:["<img src='../image/next.png'>","<img src='../image/next.png'>"],
-       responsive:{
-           0:{
-               items:1
-           },
-           600:{
-               items:3
-           },
-           1000:{
-               items:1
-           }
-       }
-   });
+    navText:["<img src='../image/next.png'>","<img src='../image/next.png'>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
 
 
 
--- js/test.js
+++ js/test.js
@@ -15,3 +15,25 @@ $('.owl-first').owlCarousel({
         }
     }
 });
+
+
+
+$('.owl-list').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:["<img src='../image/next.png'>","<img src='../image/next.png'>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
